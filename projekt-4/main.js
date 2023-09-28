9fdadeb5cc274bdf4dd4da9b4dfe1c1bc6737f2c
AniaPiWo <106525407+AniaPiWo@users.noreply.github.com>
2023-09-28 11:52:28 +0200

    4

--- projekt-4/main.js
+++ projekt-4/main.js
@@ -1,18 +1,69 @@
 const infobox = document.querySelector('.infobox');
-//const notes = document.querySelector('.notes');
+const notes = document.querySelector('.notes');
 const save_btn = document.querySelector('.save_btn');
+const textarea = document.querySelector('.note_text');
+const empty_list = document.querySelector('.empty_list');
+
+function init() {
+  const notesArray = [];
+  Object.keys(localStorage).forEach((keyNote) => {
+    const textNote = localStorage.getItem(keyNote);
+    notesArray.push({ keyNote, textNote });
+  });
+  notesArray.sort((a, b) => b.keyNote - a.keyNote);
+  notesArray.forEach(({ keyNote, textNote }) => {
+    createNote(keyNote, textNote);
+    empty_list.style.display = 'none';
+  });
+}
+init();
 
 function saveNote() {
-  let noteText = document.querySelector('.note_text').value;
-  if (noteText === '') {
+  const textNote = textarea.value;
+  const keyNote = Date.now().toString();
+  if (textNote === '') {
     infobox.innerHTML = 'You entered a task without content!';
-
     setTimeout(() => {
       infobox.innerHTML = '';
     }, 2000);
     return;
   }
-  console.log(noteText);
+
+  localStorage.setItem(keyNote, textNote);
+  createNote(keyNote, textNote);
+  textarea.value = '';
+  empty_list.style.display = 'none';
+  infobox.innerHTML = 'Task added!';
+  setTimeout(() => {
+    infobox.innerHTML = '';
+  }, 2000);
+}
+
+function deleteNote(keyNote) {
+  localStorage.removeItem(keyNote);
+  const div = document.getElementById(keyNote);
+  div.remove();
+  infobox.innerHTML = 'Task deleted!';
+  setTimeout(() => {
+    infobox.innerHTML = '';
+  }, 2000);
+}
+
+function createNote(keyNote, textNote) {
+  const div = document.createElement('div');
+  div.id = keyNote;
+  div.textContent = textNote;
+  div.className = 'text_item';
+  /*   const text = document.createTextNode(textNote);
+  div.appendChild(text); */
+  const button = document.createElement('button');
+  button.textContent = 'X';
+  button.className = 'delete_btn';
+  button.onclick = () => {
+    deleteNote(keyNote);
+  };
+  div.appendChild(button);
+  notes.appendChild(div);
 }
 
 save_btn.addEventListener('click', saveNote);
---
+++ projekt-5/main.js
@@ -0,0 +1,12 @@
+//set i map to klasy
+//set i map to struktury danych przechowujące kolekcje unikalnych wartości
+
+//set przypomina tablice ale bez indeksow
+//map przypomina tablice ale z indeksami(iterowalne) ale i obiekt (klucz wartosc)
+
+//ulatwiaja zarzadzanie kolekcjami danych
+
+//nie przechowuja duplikatow!
+
+//SET - zbiór unikalnych wartości
+//nie posiada indeksów, nie przechowuje kolejności elementów
