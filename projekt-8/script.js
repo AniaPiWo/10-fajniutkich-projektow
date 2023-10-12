6a28b8189bcbe9530b626f0e5a389f7c6b31b607
AniaPiWo <106525407+AniaPiWo@users.noreply.github.com>
2023-10-12 11:49:01 +0200

    paint

---
+++ projekt-8/script.js
@@ -0,0 +1,57 @@
+class DrawingApp {
+  constructor() {
+    this.canvas = document.getElementById('canvas');
+    this.painting = false;
+    this.#init();
+    this.#initEvents();
+  }
+
+  #init() {
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+    this.ctx = this.canvas.getContext('2d');
+    this.ctx.strokeStyle = 'white';
+    this.ctx.lineWidth = 10;
+    this.ctx.lineCap = 'round';
+  }
+
+  #initEvents() {
+    //metody na myszkę
+    this.canvas.addEventListener('mousedown', (e) => {
+      this.#startPosition(e);
+    });
+    this.canvas.addEventListener('mouseup', this.#endPosition.bind(this));
+    this.canvas.addEventListener('mousemove', this.#draw.bind(this));
+    //metody na mobile
+    this.canvas.addEventListener('touchstart', this.#startPosition.bind(this));
+    this.canvas.addEventListener('touchend', this.#endPosition.bind(this));
+    this.canvas.addEventListener('touchmove', this.#draw.bind(this));
+  }
+
+  #startPosition(e) {
+    this.painting = true;
+    this.#draw(e);
+  }
+
+  #endPosition() {
+    this.painting = false;
+    this.ctx.beginPath();
+  }
+
+  #draw(e) {
+    if (!this.painting) return;
+
+    let x = e.clientX || e.touches[0].clientX;
+    let y = e.clientY || e.touches[0].clientY;
+
+    this.ctx.lineTo(x, y);
+    this.ctx.stroke();
+    this.ctx.moveTo(x, y);
+  }
+
+  changeColor(color) {
+    this.ctx.strokeStyle = color;
+  }
+}
+
+const app = new DrawingApp();
